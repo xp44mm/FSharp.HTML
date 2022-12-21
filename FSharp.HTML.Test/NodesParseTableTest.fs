69f782790ee4abd6517dea7aab732ceb47cc83fb
xp44mm <34696643@qq.com>
2022-12-21 09:23:35 +0800

    parse table test

--- FSharp.HTML.Test/NodesParseTableTest.fs
+++ FSharp.HTML.Test/NodesParseTableTest.fs
@@ -13,6 +13,7 @@ open FSharp.xUnit
 
 open FslexFsyacc.Yacc
 open FslexFsyacc.Fsyacc
+open FslexFsyacc.Runtime
 
 type NodesParseTableTest(output:ITestOutputHelper) =
     let show res =
@@ -37,99 +38,79 @@ type NodesParseTableTest(output:ITestOutputHelper) =
     let rawFsyacc = RawFsyaccFile.parse text
     let fsyacc = FlatFsyaccFile.fromRaw rawFsyacc
 
-    [<Fact>]
-    member _.``01 - 显示冲突状态的冲突项目``() =
-        let collection =
-            fsyacc.getMainProductions()
-            |> AmbiguousCollection.create
-        let conflicts =
-            collection.filterConflictedClosures()
-        show conflicts
+    // **input**
+    let parseTblName = "NodesParseTable" 
+    let parseTblPath = Path.Combine(Dir.projPath, $"{parseTblName}.fs")
 
     [<Fact>]
-    member _.``02 - 汇总冲突的产生式``() =
-        let collection =
-            fsyacc.getMainProductions()
-            |> AmbiguousCollection.create
-        let conflicts =
-            collection.filterConflictedClosures()
-
-        let productions =
-            AmbiguousCollection.gatherProductions conflicts
-        // production -> %prec
-        let pprods =
-            ProductionUtils.precedenceOfProductions 
-                collection.grammar.terminals 
-                productions
-
-        //优先级应该据此结果给出，不能少，也不应该多。
-        let y = [
-            ]
-
-        Should.equal y pprods
-
-    [<Fact>]
-    member _.``03 - print the template of type annotaitions``() =
-        let grammar = 
+    member _.``02 - list all tokens``() =
+        let grammar =
             fsyacc.getMainProductions()
             |> Grammar.from
 
-        let symbols = 
-            grammar.symbols
-            |> Set.filter(fun x -> Regex.IsMatch(x,@"^([-\w]+|<[-\w]+>)$"))
-            |> Set.map(symbolRender)
+        let tokens = grammar.terminals
+        let res = set ["CDATA";"COMMENT";"TAGEND";"TAGSELFCLOSING";"TAGSTART";"TEXT"]
 
-        let sourceCode = 
+        //show tokens
+        Should.equal tokens res
+
+    [<Fact>]
+    member _.``03 - precedence Of Productions``() =
+        let collection = 
+            fsyacc.getMainProductions() 
+            |> AmbiguousCollection.create
+
+        let terminals = 
+            collection.grammar.terminals
+
+        let productions =
+            collection.collectConflictedProductions()
+
+        let pprods = 
+            ProductionUtils.precedenceOfProductions terminals productions
+
+        Should.equal [] pprods
+
+    [<Fact>]
+    member _.``04 - list all states``() =
+        let collection =
+            fsyacc.getMainProductions()
+            |> AmbiguousCollection.create
+        
+        let text = collection.render()
+        output.WriteLine(text)
+
+    [<Fact>]
+    member _.``05 - list the type annotaitions``() =
+        let grammar =
+            fsyacc.getMainProductions()
+            |> Grammar.from
+
+        let sourceCode =
             [
-                for i in symbols do
-                    $"{i}: \"list<string*string>\""
-            ] |> String.concat "\r\n"
+                "// Do not list symbols whose return value is always `null`"
+                "// terminals: ref to the returned type of getLexeme"
+                for i in grammar.terminals do
+                    let i = RenderUtils.renderSymbol i
+                    i + " : \"\""
+                "\r\n// nonterminals"
+                for i in grammar.nonterminals do
+                    let i = RenderUtils.renderSymbol i
+                    i + " : \"\""
+            ] 
+            |> String.concat "\r\n"
+
         output.WriteLine(sourceCode)
 
     [<Fact(Skip="once and for all!")>] //
     member _.``04 - generate parsing table``() =
-        // **input**
-        let name = "NodesParseTable" 
-        let moduleName = $"FSharp.HTML.{name}"
+        let moduleName = $"FSharp.HTML.{parseTblName}"
 
         let parseTbl = fsyacc.toFsyaccParseTableFile()
         let fsharpCode = parseTbl.generateModule(moduleName)
-        let outputDir = Path.Combine(Dir.projPath, $"{name}.fs")
 
-        File.WriteAllText(outputDir,fsharpCode)
-        output.WriteLine("output path:"+outputDir)
-
-    [<Fact>]
-    member _.``05 - list all tokens``() =
-        let grammar = 
-            fsyacc.getMainProductions()
-            |> Grammar.from
-
-        let tokens = 
-            grammar.symbols - grammar.nonterminals
-        output.WriteLine($"any={clazz tokens}")
-
-    [<Fact>]
-    member _.``06 - first or last token of node``() =
-        let grammar = 
-            fsyacc.getMainProductions()
-            |> Grammar.from
-
-        let last = grammar.lasts.["node"]
-        let first = grammar.firsts.["node"]
-
-        output.WriteLine($"last={clazz last}")
-        output.WriteLine($"first={clazz first}")
-
-    [<Fact>]
-    member _.``07 - closures``() =
-        let tbl = Compiler.parser.getParserTable()
-        let str = tbl.collection()
-
-        let name = "nodes"
-        let outputDir = Path.Combine(Dir.projPath, $"{name}.txt")
-        File.WriteAllText(outputDir,str,Encoding.UTF8)
-        output.WriteLine($"output:\r\n{outputDir}")
+        File.WriteAllText(parseTblPath,fsharpCode)
+        output.WriteLine("output path:"+parseTblPath)
 
     [<Fact>]
     member _.``08 - valid ParseTable``() =
@@ -146,12 +127,23 @@ type NodesParseTableTest(output:ITestOutputHelper) =
             FSharp.Compiler.SyntaxTreeX.SourceCodeParser.semansFromMappers mappers
 
         let header,semans =
-            let filePath = Path.Combine(Dir.projPath, "NodesParseTable.fs")
-            File.ReadAllText(filePath, Encoding.UTF8)
+            File.ReadAllText(parseTblPath, Encoding.UTF8)
             |> FSharp.Compiler.SyntaxTreeX.SourceCodeParser.getHeaderSemansFromFSharp 2
 
         Should.equal headerFromFsyacc header
         Should.equal semansFsyacc semans
+
+    [<Fact>]
+    member _.``06 - first or last token of node``() =
+        let grammar = 
+            fsyacc.getMainProductions()
+            |> Grammar.from
+
+        let last = grammar.lasts.["node"]
+        let first = grammar.firsts.["node"]
+
+        output.WriteLine($"last={clazz last}")
+        output.WriteLine($"first={clazz first}")
 
     [<Fact>]
     member _.``101 - format norm file test``() =
